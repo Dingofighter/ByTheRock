bf31fad8149d41a8f56074df5647e639b63392e9
RebeccaRomin <b15rebro@hs.local>
2017-03-30 13:29:21 +0200

    Added basic movement and temporary model/animations
    - Added basic movement script
    - Added temporary player model and movement animations
    - Edited Input for crouching

---
+++ By The Rock/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Animator anim;
+
+    bool isWalking = false;
+    bool isCrouching = false;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isWalking = !isWalking;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            isCrouching = !isCrouching;
+        }
+
+        if (isWalking)
+        {
+            anim.SetFloat("Forward", vertical * 0.5f);
+        }
+        else
+        {
+            anim.SetFloat("Forward", vertical);
+        }
+
+        anim.SetFloat("Turn", horizontal * 0.5f);
+        anim.SetBool("Crouch", isCrouching);
+    }
+}
