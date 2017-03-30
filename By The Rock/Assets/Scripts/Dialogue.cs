230484b3ebfa3af8882d7d77607bf3c36a0ff1f9
RebeccaRomin <b15rebro@hs.local>
2017-03-30 14:23:50 +0200

    Added basic dialoguehandler, player prefab

---
+++ By The Rock/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour {
+
+    public string[] lines;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public string GetLine(int lineNumber)
+    {
+        return lines[lineNumber];
+    }
+
+    public int GetLength()
+    {
+        return lines.Length;
+    }
+}
---
+++ By The Rock/Assets/Scripts/DialogueHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogueHandler : MonoBehaviour {
+
+    public Text dialogueText;
+    bool inDialogue = false;
+    Dialogue currentDialogue;
+    int currentLine = 0;
+
+    // Use this for initialization
+    void Start () {
+        dialogueText.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (inDialogue && Input.GetButtonDown("Interact"))
+        {
+            NextLine();
+        }
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        inDialogue = true;
+        currentDialogue = dialogue;
+        currentLine = 0;
+        dialogueText.text = dialogue.GetLine(currentLine);
+    }
+
+    void NextLine()
+    {
+        currentLine++;
+        if (currentLine + 1 > currentDialogue.GetLength())
+        {
+            inDialogue = false;
+            dialogueText.text = "";
+        }
+        else
+        {
+            dialogueText.text = currentDialogue.GetLine(currentLine);
+        }
+    }
+}
