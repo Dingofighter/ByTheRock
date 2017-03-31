91eec09b1a92ad85b34f759fb75a42ed54870927
Gusing <b15gusjo@hs.local>
2017-03-31 15:03:31 +0200

    More camera

--- By The Rock/Assets/WorldCamera.cs
+++ By The Rock/Assets/WorldCamera.cs
@@ -10,12 +10,110 @@ public class WorldCamera : MonoBehaviour {
 
 
 
+
+
+
+
+
+
+
+
+
+    /*
+
+    [Header("Camera Properties")]
+    public float DistanceAway;                     //how far the camera is from the player.
+    public float DistanceUp;                    //how high the camera is above the player
+    public float smooth = 4.0f;                    //how smooth the camera moves into place
+    public float rotateAround = 70f;            //the angle at which you will rotate the camera (on an axis)
+    [Header("Player to follow")]
+    public Transform target;                    //the target the camera follows
+    [Header("Layer(s) to include")]
+    public LayerMask CamOcclusion;                //the layers that will be affected by collision
+    [Header("Map coordinate script")]
+    //public worldVectorMap wvm;
+    RaycastHit hit;
+    float cameraHeight = 55f;
+    float cameraPan = 0f;
+    float camRotateSpeed = 180f;
+    Vector3 camPosition;
+    Vector3 camMask;
+    Vector3 followMask;
+    // Use this for initialization
+    void Start()
+    {
+        //the statement below automatically positions the camera behind the target.
+        rotateAround = target.eulerAngles.y - 45f;
+    }
+    void Update()
+    {
+
+    }
+    // Update is called once per frame
+
+    void LateUpdate()
+    {
+        //Offset of the targets transform (Since the pivot point is usually at the feet).
+        Vector3 targetOffset = new Vector3(target.position.x, (target.position.y + 2f), target.position.z);
+        Quaternion rotation = Quaternion.Euler(cameraHeight, rotateAround, cameraPan);
+        Vector3 vectorMask = Vector3.one;
+        Vector3 rotateVector = rotation * vectorMask;
+        //this determines where both the camera and it's mask will be.
+        //the camMask is for forcing the camera to push away from walls.
+        camPosition = targetOffset + Vector3.up * DistanceUp - rotateVector * DistanceAway;
+        camMask = targetOffset + Vector3.up * DistanceUp - rotateVector * DistanceAway;
+
+        occludeRay(ref targetOffset);
+        smoothCamMethod();
+
+        transform.LookAt(target);
+
+        #region wrap the cam orbit rotation
+        if (rotateAround > 360)
+        {
+            rotateAround = 0f;
+        }
+        else if (rotateAround < 0f)
+        {
+            rotateAround = (rotateAround + 360f);
+        }
+        #endregion
+
+        rotateAround += Input.GetAxis("Mouse X") * camRotateSpeed * 0.07f; //* Time.deltaTime;
+        DistanceUp = Mathf.Clamp(DistanceUp += Input.GetAxis("Mouse Y"), -0.79f, 2.3f);
+    }
+    void smoothCamMethod()
+    {
+        smooth = 4f;
+        transform.position = Vector3.Lerp(transform.position, camPosition, Time.deltaTime * smooth);
+    }
+    void occludeRay(ref Vector3 targetFollow)
+    {
+        #region prevent wall clipping
+        //declare a new raycast hit.
+        RaycastHit wallHit = new RaycastHit();
+        //linecast from your player (targetFollow) to your cameras mask (camMask) to find collisions.
+        if (Physics.Linecast(targetFollow, camMask, out wallHit, CamOcclusion))
+        {
+            //the smooth is increased so you detect geometry collisions faster.
+            smooth = 10f;
+            //the x and z coordinates are pushed away from the wall by hit.normal.
+            //the y coordinate stays the same.
+            camPosition = new Vector3(wallHit.point.x + wallHit.normal.x * 0.5f, camPosition.y, wallHit.point.z + wallHit.normal.z * 0.5f);
+        }
+        #endregion
+    }
+
+    */
+
     
 
 
-
+    
     
     public Transform target;
+    public Transform maxTarget;
+    public float height = 5.0f;
     public float distance = 5.0f;
     public float xSpeed = 120.0f;
     public float ySpeed = 120.0f;
@@ -26,7 +124,9 @@ public class WorldCamera : MonoBehaviour {
     public float distanceMin = .5f;
     public float distanceMax = 15f;
 
-    private Rigidbody rigidbody;
+    float desiredDistance = 5.0f;
+
+   // private Rigidbody rigidbody;
     private Vector3 camMax;
 
     float x = 0.0f;
@@ -41,13 +141,13 @@ public class WorldCamera : MonoBehaviour {
         x = angles.y;
         y = angles.x;
 
-        rigidbody = GetComponent<Rigidbody>();
+        //rigidbody = GetComponent<Rigidbody>();
 
         // Make the rigid body not change rotation
-        if (rigidbody != null)
+        /*if (rigidbody != null)
         {
             rigidbody.freezeRotation = true;
-        }
+        }*/
 
         hitDistance = 0;
         camMax = transform.position;
@@ -57,45 +157,79 @@ public class WorldCamera : MonoBehaviour {
     {
         if (target)
         {
-            x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.05f;
             y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+
+            desiredDistance = Mathf.Clamp(desiredDistance, distanceMin, distanceMax);
+
+            distance = desiredDistance;
+
 
             y = ClampAngle(y, yMinLimit, yMaxLimit);
 
             Quaternion rotation = Quaternion.Euler(y, x, 0);
 
+
+            Vector3 position = target.position - (rotation * Vector3.forward * distance);            // Collision Detection
+            RaycastHit collisionHit;
+            Vector3 cameraTargetPosition = new Vector3(target.position.x, target.position.y + height, target.position.z);
+            if (Physics.Linecast(cameraTargetPosition, position, out collisionHit))
+            {
+                position = collisionHit.point;
+                distance = Vector3.Distance(target.position, position);
+            }
+            transform.rotation = rotation;
+            transform.position = position;
+
+
+
+
+
+            /*
+
             //if (distance >= 5) camMax = transform.position;
 
-            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+            //distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+            desiredDistance = Mathf.Clamp(desiredDistance, distanceMin, distanceMax);
+            distance = desiredDistance;
             
-
             RaycastHit hit;
             if (Physics.Linecast(target.position, transform.position, out hit))
             {
-                distance -= hit.distance;
+                distance = hit.distance;
                 hitDistance = hit.distance;
             }
-            if (distance < 5)
+            //else distance = 5;
+            else if (distance < 5)
             {
-                if (Physics.Linecast(target.position, camMax, out hit))
+                if (Physics.Linecast(transform.position, camMax, out hit))
                 {
-                    distance += hit.distance-hitDistance;
+                    //distance += hit.distance-hitDistance;
+                    //distance += (5-distance)-hit.distance;
+                    distance = hit.distance;
+
                 }
                 else
                 {
                     distance = 5;
                 }
             }
+            else
+            {
+                //distance = desiredDistance;
+            }
 
             Mathf.Clamp(distance, distanceMin, distanceMax);
-            
+
             Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
-            Vector3 position = rotation * negDistance + target.position;
+            Vector3 position = rotation * negDistance + new Vector3(target.position.x, target.position.y + height, target.position.z);
 
             camMax = rotation * new Vector3(0.0f, 0.0f, -5.0f) + target.position;
+            maxTarget.position = camMax;
             
             transform.rotation = rotation;
-            transform.position = position;
+            transform.position = position;*/
         }
     }
 
@@ -107,9 +241,6 @@ public class WorldCamera : MonoBehaviour {
             angle -= 360F;
         return Mathf.Clamp(angle, min, max);
     }
-    
-
-
 
 
 
