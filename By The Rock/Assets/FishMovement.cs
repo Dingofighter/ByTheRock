336c602715710879539f975c0738178c423c77d0
Gusing <b15gusjo@hs.local>
2017-04-24 17:15:16 +0200

    Water plus fishes
    :hamster:

---
+++ By The Rock/Assets/FishMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishMovement : MonoBehaviour {
+
+    public int rotateSpeed;
+    public float moveSpeed;
+
+    public ParticleSystem splash;
+
+    int timer;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //transform.position += transform.right * 0.1f;
+
+        //transform.position = new Vector3(transform.position.x, transform.position.y + 0.2f * Mathf.Sin(Time.time * 3), transform.position.z);
+
+        timer++;
+        if (timer == 50)
+        {
+            //splash.Play();
+            timer = 0;
+        }
+        
+        //transform.position -= transform.forward * 0.2f * Mathf.Sin(Time.time * 3);
+        transform.Rotate(-Vector3.up * Time.deltaTime*rotateSpeed);
+
+        Debug.Log(transform.rotation.eulerAngles);
+
+        if (transform.rotation.eulerAngles.x > 315 && transform.rotation.eulerAngles.x < 325) splash.Play();
+
+        //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y+100, transform.rotation.z, transform.rotation.w);
+        transform.position -= transform.right * moveSpeed;
+       // transform.rotation
+
+    }
+}
