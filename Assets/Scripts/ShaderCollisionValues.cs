40e1e04a1517fdfc14eb9dc6b9ca323ef32639f8
Wiive <a15loubu@hs.local>
2017-04-27 13:50:38 +0200

    Added skybox and trees

---
+++ Assets/Scripts/ShaderCollisionValues.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderCollisionValues : MonoBehaviour {
+
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Shader.SetGlobalVector("_PlayerPos", new Vector4(player.position.x, player.position.y, player.position.z, 0));
+	}
+}
