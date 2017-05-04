48091d21db4a2cb250a1f29b90a34f86ea008b81
HS\b15ponjo <pontusjohansson@live.com>
2017-05-04 17:02:34 +0200

    Hello

--- By The Rock/Assets/Scripts/SoundScripts/FootstepsEmitter.cs
+++ By The Rock/Assets/Scripts/SoundScripts/FootstepsEmitter.cs
@@ -3,5 +3,22 @@ using System.Collections;
 
 public class FootstepsEmitter : BaseEmitter {
 
+    Transform _t;
 
+    protected override void Start()
+    {
+        _TriggerOnce = true;
+        _t = this.transform;
+        base.Start();
+    }
+
+    public void PlayStep(int stepType)
+    {
+        Debug.Log("FOOTSTEPSOUND" + stepType);
+        _3dAttributes.position.x = _t.position.x;
+        _3dAttributes.position.y = _t.position.y;
+        _3dAttributes.position.z = _t.position.z;       
+        _EventInstance.start();
+        _EventInstance.set3DAttributes(_3dAttributes);
+    }
 }
--- By The Rock/Assets/Scripts/SoundScripts/MusicEmitter.cs
+++ By The Rock/Assets/Scripts/SoundScripts/MusicEmitter.cs
@@ -13,6 +13,7 @@ public class MusicEmitter : BaseEmitter {
         base.Start();
         _paramBincrement = "SmallPassage";
         _paramBool = "Bool";
+        Debug.Log(_EventInstance);
         _EventInstance.setParameterValue(_paramBincrement, -1);
         _EventInstance.setParameterValue(_paramBool, 0);
     }
