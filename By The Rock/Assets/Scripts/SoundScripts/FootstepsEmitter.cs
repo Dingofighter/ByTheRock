f5883329fd79992ba4bb82b8eaf0d6d782cd27be
danielmichaneck <daniel.michaneck@gmail.com>
2017-05-17 20:51:49 +0200

    Revert "Pontus Dialogue Script & Footsteps update"
    This reverts commit f2a07cbbb19b05ea5bfdf3e3a1dd688fed3e6245.

--- By The Rock/Assets/Scripts/SoundScripts/FootstepsEmitter.cs
+++ By The Rock/Assets/Scripts/SoundScripts/FootstepsEmitter.cs
@@ -1,22 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FootstepsEmitter : BaseEmitter
-{
+public class FootstepsEmitter : BaseEmitter {
 
-    protected override void Start()
-    {
-        base.Start();
-    }
 
-    public void PlayStep(int stepnr)
-    {
-        var _t = this.transform.position;
-        _EventInstance.start();
-        _3dAttributes.position.x = _t.x;
-        _3dAttributes.position.y = _t.y;
-        _3dAttributes.position.z = _t.z;
-        _EventInstance.set3DAttributes(_3dAttributes);
-        Debug.Log("Step nr: " + stepnr);
-    }
 }
