c3053f35f11e4f6ef2a10eaae3bd731fe0a8d4bd
Gusing <b15gusjo@hs.local>
2017-05-19 20:56:32 +0200

    Fire at the menu kinda
    :fire: :fire: :100:

--- By The Rock/Assets/Scripts/LoadSceneOnClick.cs
+++ By The Rock/Assets/Scripts/LoadSceneOnClick.cs
@@ -4,10 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneOnClick : MonoBehaviour {
 
+    float startTimer;
+
+    public GameObject theCamera;
+
 	public void loadByIndex(int sceneIndex)
     {
+        startTimer += Time.deltaTime * 60;
+        theCamera.transform.position = new Vector3(theCamera.transform.position.x, theCamera.transform.position.y, theCamera.transform.position.z + 50);
+
+
         Time.timeScale = 1.0f;
         //GameManager.instance.paused = true;
-        SceneManager.LoadScene(sceneIndex);
+        //SceneManager.LoadScene(sceneIndex);
     } 
 }
