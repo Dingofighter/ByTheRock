5234d88bcb45acddada326ded906a04ff50250a2
Skugg <a15karka@hs.local>
2017-05-25 16:50:46 +0200

    Skapade Karls coola testscript

---
+++ By The Rock/Assets/KarlsCoolaTestScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KarlsCoolaTestScript : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("KarleBest!");
+            Screen.SetResolution(1920, 1080, true);
+        }
+    }
+}
